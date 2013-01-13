ebf8bd1ff24946b49b1b3a35b3bad421e8a7ca4b
Fernando Arean <farean@gmail.com>
2013-01-12 23:46:55 -0300

    First Commit

---
+++ MyDashboard.Model.Entities/Interfaces/IProject.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MyDashboard.Model.Entities.Interfaces
+{
+    interface IProject
+    {
+        int ID { get; set; }
+        string Name { get; set; }
+    }
+}
---
+++ MyDashboard.Model.Entities/Project.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyDashboard.Model.Entities.Interfaces;
+namespace MyDashboard.Model.Entities
+{
+    public class Project : IProject
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
